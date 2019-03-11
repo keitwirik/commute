2a7b4487a4fedb206078e3ffce96d1a776679573
keit <brian@pseudoscope.com>
2019-03-11 10:20:20 -0500

    clean up router

--- src/modules/commuteRouter.js
+++ src/modules/commuteRouter.js
@@ -1,13 +1,13 @@
-import busRoutes from './bus_routes/bus_routes';
-import busDirections from './bus_directions/bus_directions';
-import busStops from './bus_stops/bus_stops';
-import busStopPredictions from './bus_predictions/bus_predictions';
-import trainLines from './train_lines/train_lines';
-import trainStops from './train_stops/train_stops';
-import trainStopPredictions from './train_stop_predictions/train_stop_predictions';
-import trainFollow from './train_follow/train_follow';
+import busRoutes from "./bus_routes/bus_routes";
+import busDirections from "./bus_directions/bus_directions";
+import busStops from "./bus_stops/bus_stops";
+import busStopPredictions from "./bus_predictions/bus_predictions";
+import trainLines from "./train_lines/train_lines";
+import trainStops from "./train_stops/train_stops";
+import trainStopPredictions from "./train_stop_predictions/train_stop_predictions";
+import trainFollow from "./train_follow/train_follow";
 
-var parent = document.querySelector('.info');
+var parent = document.querySelector(".info");
 
 const Commute = {
   Views: {},
@@ -15,7 +15,7 @@ const Commute = {
 };
 
 const busdirection = props => {
-  console.log('bus route', props);
+  console.log("bus route", props);
 };
 
 const routes = {
@@ -31,8 +31,8 @@ const routes = {
 };
 
 const matchUrl = url => {
-  if (url === '') {
-    return { function: 'home' };
+  if (url === "") {
+    return { function: "home" };
   }
   for (var route in routes) {
     const match = url.match(routes[route]);
@@ -44,132 +44,56 @@ const matchUrl = url => {
       return everything;
     }
   }
-  return 'nuthin';
+  return "nuthin";
 };
 
 Commute.navigate = page => {
-  console.log('navigating', page);
+  console.log("navigating", page);
   history.pushState(null, null, page);
-  page = page.replace(/^\//, '').replace(/\/$/, ''); // shave off slashs
+  page = page.replace(/^\//, "").replace(/\/$/, ""); // shave off slashs
   const next = matchUrl(page);
-  console.log('next', next);
-  if (next.function === 'home') {
+  console.log("next", next);
+  if (next.function) {
     while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
     }
-    // show recent trips
-    console.log('home page');
   }
-  if (next.function === 'busRoutes') {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('bus routes dispatch');
+  if (next.function === "home") {
+    // show recent trips
+    console.log("home page");
+  }
+  if (next.function === "busRoutes") {
+    console.log("bus routes dispatch");
     busRoutes();
   }
-  if (next.function === 'busDirections' && next.props[1]) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('bus directions dispatch', next.props[1]);
+  if (next.function === "busDirections" && next.props[1]) {
+    console.log("bus directions dispatch", next.props[1]);
     busDirections(next.props[1]);
   }
-  if (next.function === 'busStops' && next.props[1] && next.props[2]) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('busStops dispatch', next.props);
+  if (next.function === "busStops" && next.props[1] && next.props[2]) {
+    console.log("busStops dispatch", next.props);
     busStops(next.props);
   }
-  if (next.function === 'busStopPredictions' && next.props[1]) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('busStopPredictions dispatch', next.props);
+  if (next.function === "busStopPredictions" && next.props[1]) {
+    console.log("busStopPredictions dispatch", next.props);
     busStopPredictions(next.props);
   }
-  if (next.function === 'trainLines') {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('train lines dispatch');
+  if (next.function === "trainLines") {
+    console.log("train lines dispatch");
     trainLines();
   }
-  if (next.function === 'trainStops') {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('train stops dispatch');
+  if (next.function === "trainStops") {
+    console.log("train stops dispatch");
     trainStops();
   }
-  if (next.function === 'trainStopPredictions') {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('train stop predictions dispatch');
+  if (next.function === "trainStopPredictions") {
+    console.log("train stop predictions dispatch");
     trainStopPredictions(next.props);
   }
-  if (next.function === 'trainFollow') {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    console.log('train follow dispatch', next.props);
+  if (next.function === "trainFollow") {
+    console.log("train follow dispatch", next.props);
     trainFollow(next.props);
   }
 };
-
-//let url = '/bus/r/5/north/'; // starting path
-//url = url.replace(/^\//, '').replace(/\/$/, ''); // shave off slashes
-
-//navigate(); //do this on url change
-
-// Commute.Router = Backbone.Router.extend({
-//   routes: {
-//     '': 'index',
-//     bus: 'getRoutes',
-//     'bus/r/:routeId': 'getDirections',
-//     'bus/r/:routeId/:direction': 'getStops',
-//     'bus/s/:stopId': 'getPrediction',
-//     trains: 'getTrainlines',
-//     'trains/r/:trainline': 'getTrainStops',
-//     'trains/r/:trainline/s/:stopId': 'getTrainPrediction'
-//   },
-//
-// getRoutes: function() {
-//   //new Commute.Views.ListBusRoutesView();
-//   while (parent.firstChild) {
-//     parent.removeChild(parent.firstChild);
-//   }
-//   busRoutes();
-// },
-//
-//   getDirections: function(routeId) {
-//     // new Commute.Views.BusDirectionsView(routeId);
-//     while (parent.firstChild) {
-//       parent.removeChild(parent.firstChild);
-//     }
-//     busDirections();
-//   },
-//
-//   getStops: function(routeId, direction) {
-//     new Commute.Views.BusStopsView(routeId, direction);
-//   },
-//
-//   getPrediction: function(stopId) {
-//     new Commute.Views.BusPredictions(stopId);
-//   },
-//
-//   getTrainlines: function() {
-//     new Commute.Views.ListTrainLinesView();
-//   },
-//
-//   getTrainStops: function(trainline) {
-//     new Commute.Views.TrainStopsView(trainline);
-//   },
-//
-//   getTrainPrediction: function(trainline, stopId) {
-//     new Commute.Views.TrainPredictionView(trainline, stopId);
-//   }
-//  });
 
 export default Commute;
